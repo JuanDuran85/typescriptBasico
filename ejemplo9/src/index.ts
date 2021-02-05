a1f435ee8ccd1952a53a33bb8d3c92db5cdea29f
= <duran85juan@mail.com>
2021-02-05 16:25:55 -0300

    trabajando con funciones

--- ejemplo9/src/index.ts
+++ ejemplo9/src/index.ts
@@ -1,3 +1,19 @@
 /*
     ===== Código de TypeScript =====
 */
+
+function sumar(a:number, b:number) : number {
+    return a+b;
+}
+
+let sumarFlecha = (a:number, b:number) : number => a+b;
+
+let resultado : number = sumar(4,6);
+console.log(resultado);
+console.log(sumarFlecha(3,7));
+
+function multiplicar(numA:number, numB?: number, numC: number = 5) {
+    return numA*numC;
+};
+
+console.log(multiplicar(3));
