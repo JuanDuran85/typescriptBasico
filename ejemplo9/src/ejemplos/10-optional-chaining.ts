3b6ded276e28a2b367c93de0013c836ffb25e6ae
JuanDuran85 <duran85juan@gmail.com>
2021-02-09 16:00:48 -0300

    nuevos cambios.

---
+++ ejemplo9/src/ejemplos/10-optional-chaining.ts
@@ -0,0 +1,36 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+// un decorador es una funcion que expande su clase añadiendo funcionalidades especiales. 
+
+class MiSuperClase {
+    public miPropiedad: string = 'ABC';
+    
+    imprimirPropiedad(){
+        console.log(this.miPropiedad);
+    }
+}
+
+// encadenamiento opcional
+
+interface Pasajero {
+    nombre: string;
+    hijos?: string[]; // ? propiedad opcional
+}
+
+const pasajero1 : Pasajero = {
+    nombre: "Juan"
+}
+
+const pasajero2 : Pasajero = {
+    nombre: "Yecenia",
+    hijos: ["Ali", "Coromoto"]
+}
+
+function imprimeHijos(pasajero: Pasajero) : void {
+    const numeroHijos = pasajero.hijos?.length || 0;
+    console.log(numeroHijos);
+}
+
+imprimeHijos(pasajero1);
--- ejemplo9/src/index.ts
+++ ejemplo9/src/index.ts
@@ -1,13 +1,3 @@
 /*
     ===== Código de TypeScript =====
 */
-
-// un decorador es una funcion que expande su clase añadiendo funcionalidades especiales. 
-
-class MiSuperClase {
-    public miPropiedad: string = 'ABC';
-    
-    imprimirPropiedad(){
-        console.log(this.miPropiedad);
-    }
-}
