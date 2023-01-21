b23386f90e9fed409b95f6ccbeb4b17665fa27a7
Juan <duran85juan@gmail.com>
2023-01-20 23:00:23 -0300

    class typescript

--- ejemplo22/src/app.ts
+++ ejemplo22/src/app.ts
@@ -1,8 +1,37 @@
 class Departament {
-    name: string;
 
-    constructor(n: string){
-        this.name = n;
+    private employees: string[] = [];
+
+    // You can use a short definition on the constructor
+    constructor(public name: string, private readonly idUser: string){}
+
+
+    describe(this: Departament){
+        console.log(`Department: ${this.name}`);
+        console.log(`Id User: ${this.idUser}`);
+    }
+
+    addEmployees(employee: string){
+        this.employees.push(employee);
+    }
+
+    showEmployeeInformation(){
+        console.log(`Employees: ${this.employees}`);
     }
 }
+
+const accounting = new Departament("Nombre",'34532121ADF');
+accounting.describe();
+accounting.addEmployees("Empleado 1");
+accounting.addEmployees("Empleado 2");
+accounting.addEmployees("Empleado 3");
+accounting.showEmployeeInformation()
+
+const accountingCopy = {
+    name: "DUMMY",
+    describe: accounting.describe
+}
+
+// accountingCopy.describe();
+
 
