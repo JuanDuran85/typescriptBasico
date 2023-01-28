3400ec4186a50eb72529248fa2039f2d52115207
Juan <duran85juan@gmail.com>
2023-01-27 23:11:02 -0300

    working with classes

--- ejemplo22/src/app.ts
+++ ejemplo22/src/app.ts
@@ -1,10 +1,11 @@
 class Departament {
 
     private employees: string[] = [];
+    // when you use protected for a property, that property is available in the other class
+    protected budget: number[] = [];
 
     // You can use a short definition on the constructor
     constructor(public name: string, private readonly idUser: string){}
-
 
     describe(this: Departament){
         console.log(`Department: ${this.name}`);
@@ -20,18 +21,53 @@ class Departament {
     }
 }
 
-const accounting = new Departament("Nombre",'34532121ADF');
-accounting.describe();
-accounting.addEmployees("Empleado 1");
-accounting.addEmployees("Empleado 2");
-accounting.addEmployees("Empleado 3");
-accounting.showEmployeeInformation()
-
-const accountingCopy = {
-    name: "DUMMY",
-    describe: accounting.describe
+class ItDepartament extends Departament{
+    constructor(id: string, public admins: string[]){
+        super(id, 'IT')
+    }
 }
 
+class AccountingDepartament extends Departament{
+    constructor(id: string, private reports: string[]){
+        super(id, 'Accounting');
+    }
+
+    addReports(reportText: string){
+        this.reports.push(reportText);;
+    }
+
+    getReports(){
+        console.log(this.reports);
+    }
+
+    addBudget(newBudget: number) {
+        if (newBudget < 1000) {
+            console.error("Error: new budget")
+        }
+        this.budget.push(newBudget);
+    }
+}
+
+const iTDepart = new ItDepartament("1a3S234d62",['Juan']);
+iTDepart.describe();
+iTDepart.addEmployees("Empleado 1");
+iTDepart.addEmployees("Empleado 2");
+iTDepart.addEmployees("Empleado 3");
+iTDepart.showEmployeeInformation()
+
+const iTDepartCopy = {
+    name: "DUMMY",
+    describe: iTDepart.describe
+}
+
+const accounting = new AccountingDepartament("43Fdd3sD",[]);
+
+accounting.addBudget(3456);
+accounting.addReports("Errors number one");
+console.log({ accounting});
+
 // accountingCopy.describe();
+
+console.log({iTDepart});
 
 
