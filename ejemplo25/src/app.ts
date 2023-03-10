4c0dafb1855727f05d8f32c4c54af5799b1bf3fd
Juan <duran85juan@gmail.com>
2023-03-10 09:11:07 -0300

    partial type

--- ejemplo25/src/app.ts
+++ ejemplo25/src/app.ts
@@ -124,8 +124,27 @@ displayUserData();
 // Partial
 // Constructs a type with all properties of Type set to optional. This utility will return a type that represents all subsets of a given type.
 
+interface UsersInfo {
+  id: string;
+  name: string;
+  lastName: string;
+  age: number;
+}
 
+const infoUsurio: UsersInfo = {
+  id: '',
+  name: '',
+  lastName: '',
+  age: 0,
+};
 
+console.log(infoUsurio);
+
+const usuarios = (infoIn: UsersInfo, infoToUpdate: Partial<UsersInfo>) => {
+  return { ... infoIn, ...infoToUpdate }; 
+}
+
+console.log(usuarios(infoUsurio,{ name: 'Juan', lastName: 'Duran', age: 37}));
 
 
 
