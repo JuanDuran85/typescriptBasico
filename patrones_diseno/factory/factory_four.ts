42b9e22117c4b23a65fe833192b7a3f1135b57e1
Juan <duran85juan@gmail.com>
2023-03-19 19:55:09 -0300

    factory pattern

---
+++ patrones_diseno/factory/factory_four.ts
@@ -0,0 +1,62 @@
+/**
+ * 
+ * The factory method pattern, also known as the factory pattern, is also called the polymorphic
+ * factory pattern, which belongs to the creational pattern.
+ * 
+ * In the factory method pattern, the factory parent class is responsible for defining the public
+ * interface for creating product objects, while the factory subclass is responsible for generating
+ * specific product objects. The purpose of this is to delay the instantiation of the product class to
+ * the factory subclass. That is, through the factory subclass to determine which specific product
+ * class should be instantiated.
+ * 
+ */
+
+// 1. Define an abstract class Vehicle and its two subclasses VehicleSuperX01 and VehicleSuperX02 to represent different types of vehicles.
+abstract class VehicleNew {
+    abstract run(): void;
+}
+
+class VehicleNewSuperX01 extends VehicleNew {
+    run(): void {
+        console.log("VehicleSuperX01 init");
+    }
+}
+
+class VehicleNewSuperX02 extends VehicleNew {
+    run(): void {
+        console.log("VehicleSuperX02 init");
+    }
+}
+
+// Define the VehicleFactory class to represent the vehicle factory. The abstract class contains an abstract method produceVehicle, which is the so-called factory method.
+
+abstract class VehicleNewFactory {
+    abstract produceVehicle(): VehicleNew
+}
+
+// Based on the VehicleNewFactory abstract class... Define the VehicleNewSuperX01Factory and VehicleNewSuperX02Factory factory classes for the production of SuperX01 and SuperX02 models of vehicles
+
+class VehicleNewSuperX01Factory extends VehicleNewFactory {
+    produceVehicle(): VehicleNew {
+        console.log("Vehicle New SuperX01 start to produce vehicle");
+        return new VehicleNewSuperX01();
+    }
+}
+
+class VehicleNewSuperX02Factory extends VehicleNewFactory {
+    produceVehicle(): VehicleNew {
+        console.log("Vehicle New SuperX02 start to produce vehicle");
+        return new VehicleNewSuperX02();
+    }
+}
+
+// Finally, after creating the VehicleNewSuperX01Factory and VehicleNewSuperX02Factory factory classes, you can start producing cars.
+
+const vehicleNewSuperX01Factory: VehicleNewSuperX01Factory = new VehicleNewSuperX01Factory();
+const vehicleNewSuperX02Factory: VehicleNewSuperX02Factory = new VehicleNewSuperX02Factory();
+
+const vehicleNewSuperX01 = vehicleNewSuperX01Factory.produceVehicle();
+const vehicleNewSuperX02 = vehicleNewSuperX02Factory.produceVehicle();
+
+vehicleNewSuperX01.run();
+vehicleNewSuperX02.run();
---
+++ patrones_diseno/factory/factory_three.ts
@@ -0,0 +1,46 @@
+/**
+ * 
+ * Simple Factory Pattern
+ * 
+ * The simple factory pattern is also called the static method pattern, because a static method is
+ * defined in the factory class to create an object. Simple factories allow users to create the desired
+ * “product” class without knowing the specific parameters, that is, the user can directly consume the
+ * product without knowing the specific production details of the product.
+ * 
+ */
+
+
+
+// simple factory to describe the process of producing a given model of car in a vehicle factory.
+
+// 1. Define an abstract class Vehicle and its two subclasses VehicleSuperX01 and VehicleSuperX02 to represent different types of vehicles.
+abstract class Vehicle {
+    abstract run(): void;
+}
+
+class VehicleSuperX01 extends Vehicle {
+    run(): void {
+        console.log("VehicleSuperX01");
+    }
+}
+
+class VehicleSuperX02 extends Vehicle {
+    run(): void {
+        console.log("VehicleSuperX02");
+    }
+}
+
+//2. define the VehiculeSuperXFactory class to represent the vehicle factory.
+
+const vhSx01: VehicleSuperX01 = new VehicleSuperX01();
+vhSx01.run();
+const vhSx02: VehicleSuperX02 = new VehicleSuperX02();
+vhSx02.run();
+
+
+/*  
+The usage scenarios of the simple factory pattern:
+
+- The factory class is responsible for creating fewer objects: Since there are fewer objects created, the business logic in the factory method will not be too complicated.
+- The client only needs to know the parameters passed to the static method of the factory class, and does not need to care about the details of creating an object.
+*/
