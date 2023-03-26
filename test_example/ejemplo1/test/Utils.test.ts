5a5c5aeaa2cf1c06d671f205ae4ab80219b75b2b
Juan <duran85juan@gmail.com>
2023-03-26 16:15:24 -0300

    unit test with jest  and typescript

--- test_example/ejemplo1/test/Utils.test.ts
+++ test_example/ejemplo1/test/Utils.test.ts
@@ -1,8 +1,27 @@
 import { dumpToUperCase } from "../src/Utils";
 
+//--------------------------------------------
+/*   
+    Structure of a property written unit test
+  AAA principles:
+   - arrange
+   - act
+   - assert
+*/
+//--------------------------------------------
+
 describe("Dump Utils test suite", () => {
-  test("should return upperCase", () => {
-    const result = dumpToUperCase("abcdef");
-    expect(result).toBe("ABCDEF");
+  it("should return upperCase of valid string", () => {
+    // arrange
+
+    // create a System Under Test
+    const sut: (args: string) => string = dumpToUperCase;
+    const expected: string = "ABCDEF";
+
+    // act
+    const actual: string = sut("abcdef");
+
+    // assert
+    expect(actual).toBe(expected);
   });
 });
