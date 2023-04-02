e22d1ceaa691b2935a9f9ef89e039231d937829e
Juan <duran85juan@gmail.com>
2023-04-02 12:35:26 -0400

    qorking with example

--- drap_and_drop_project_example/src/app.ts
+++ drap_and_drop_project_example/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   public templateElement: HTMLTemplateElement;
   public hostElement: HTMLDivElement;
   public elementForm: HTMLFormElement;
+  public titleInputElement: HTMLInputElement;
+  public descriptionInputElement: HTMLInputElement;
+  public peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +17,29 @@ class ProjectInput {
       true
     );
     this.elementForm = importedNodeHtml.firstElementChild as HTMLFormElement;
+    this.elementForm.id = "user-input";
+
+    this.titleInputElement = this.elementForm.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.elementForm.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.elementForm.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+    
+    this.configurate();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configurate() {
+    this.elementForm.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
