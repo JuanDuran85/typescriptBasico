082a54d56b15df8819b1fdd3f406f053b4b31514
Juan <duran85juan@gmail.com>
2023-04-28 23:40:28 -0400

    working with class-validator

---
+++ using-libraries/src/class_validator/index.ts
@@ -0,0 +1,35 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  ValidationError,
+  validate,
+} from "class-validator";
+
+export class Products {
+  @IsNotEmpty()
+  public title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  public price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+
+  getInformation() {
+    return [this.title, `$${this.price}`];
+  }
+}
+
+const istanceOne = new Products("", -4);
+validate(istanceOne).then((result: ValidationError[]) => {
+  if (result.length > 0) {
+    console.info(result);
+    throw new Error("Errores");
+  }
+  console.log(istanceOne);
+});
