554a2cf2eb43a3dade7a4bb2c30150a1ac36ffbe
Juan <duran85juan@gmail.com>
2023-04-29 19:37:11 -0400

    working with map

--- select_share_places_ex/src/app.ts
+++ select_share_places_ex/src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+import axios from 'axios';
+
+
+// getting input form
+
+const form: HTMLFormElement = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    console.log(enteredAddress);
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_NUMBER}
+    `).then((response)=>{
+        console.log(response);
+    }).catch(error =>{
+        console.log(error);
+    });
+}
+
+form.addEventListener("submit",searchAddressHandler);
