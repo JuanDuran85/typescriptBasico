544fbfb0b259e018bcc0cc65a5f924f4e036472a
Juan <duran85juan@gmail.com>
2023-05-13 00:02:58 -0400

    working with singleton pattern

---
+++ patrones_diseno/singleton/singleton_three.ts
@@ -0,0 +1,18 @@
+class SingletonThree {
+  private static instance: SingletonThree;
+  public random: number;
+
+  private constructor() {
+    this.random = Math.random();
+  }
+
+  public static getInstance(): SingletonThree {
+    if (!this.instance) this.instance = new SingletonThree();
+    return this.instance;
+  }
+}
+
+const singletonClass = SingletonThree.getInstance();
+const singletonClass2 = SingletonThree.getInstance();
+console.log(singletonClass);
+console.log(singletonClass2);
