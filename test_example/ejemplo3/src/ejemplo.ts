360db0e44499c5ef96f8a1e4fdbdb120022f0d70
JuanDuran85 <duran85juan@gmail.com>
2023-10-08 18:52:16 -0300

    working with unittest on TS

--- test_example/ejemplo3/src/ejemplo.ts
+++ test_example/ejemplo3/src/ejemplo.ts
@@ -1,43 +1,57 @@
-export function processLogsX(logs: string[], threshold: number){
-    if(!Number(threshold) && threshold < 1 ) {
-        throw new Error()
+export function processLogsX(logs: string[], threshold: number) {
+  if (!Number(threshold) && threshold < 1) {
+    throw new Error("Error...");
+  }
+
+  if (!logs || logs.length < 1 || !Array.isArray(logs)) {
+    throw new Error("Error...");
+  }
+
+  const firstIDCount = {};
+  const finalCountResult = [];
+
+  for (const log of logs) {
+    const [sender_user_id, recipient_user_id, amount_of_transactions] =
+      log.split(" ");
+
+    if (!sender_user_id || !recipient_user_id || !amount_of_transactions) {
+      throw new Error();
     }
 
-    if(!logs && logs.length < 1 || !Array.isArray(logs)) {
-        throw new Error()
+    if (
+      !Number(sender_user_id) ||
+      !Number(recipient_user_id) ||
+      !Number(amount_of_transactions)
+    )
+      throw new Error("Error...");
+
+
+    if(sender_user_id.startsWith('0') || recipient_user_id.startsWith('0') || amount_of_transactions.startsWith('0')) {
+      throw new Error("Error...");
     }
 
-    const firstIDCount = {};
-    const secondIDCount = {};
-    let countResult = {};
-    const finalCountResult = []
-
-    for (const log of logs) {
-        const [sender_user_id, recipient_user_id, amount_of_transactions] = log.split(" ");
-
-        if(!sender_user_id || !recipient_user_id || !amount_of_transactions) {
-            throw new Error()
-        }
-
-        firstIDCount[sender_user_id] = (firstIDCount[sender_user_id] || 0) + 1;
-        secondIDCount[recipient_user_id] = (secondIDCount[recipient_user_id] || 0) + 1;
+    if (sender_user_id === recipient_user_id) {
+      firstIDCount[recipient_user_id] =
+        (firstIDCount[recipient_user_id] || 0) - 1;
     }
 
-    countResult = { ...firstIDCount, ...secondIDCount };
+    firstIDCount[sender_user_id] = (firstIDCount[sender_user_id] || 0) + 1;
+    firstIDCount[recipient_user_id] =
+      (firstIDCount[recipient_user_id] || 0) + 1;
+  }
 
-    for (const [key, value] of Object.entries(countResult)) {
-        //@ts-ignore
-        if (value >= threshold) {
-            finalCountResult.push({
-                id: key,
-                count: value
-            });
-        }
+  for (const [key, value] of Object.entries(firstIDCount)) {
+    //@ts-ignore
+    if (value >= threshold) {
+      finalCountResult.push({
+        id: key,
+        count: value,
+      });
     }
+  }
 
-    return finalCountResult.map(({id, count}) => id);
+  return finalCountResult.map(({ id, count }) => id);
 }
 
-const result = processLogsX(["1 2 50","1 7 70", "1 3 20", "2 2 17"],2);
-console.debug("🚀 ~ file: ejemplo.ts:49 ~ result:", result)
-
+// const result = processLogsX(["1 2 50","1 7 70", "1 3 20", "2 2 17"],2);
+// console.debug("🚀 ~ file: ejemplo.ts:49 ~ result:", result)
--- test_example/ejemplo3/test/ejemplo.test.ts
+++ test_example/ejemplo3/test/ejemplo.test.ts
@@ -1,59 +1,40 @@
 import { processLogsX } from "../src/ejemplo";
 
+
+let sut: (logs: string[], threshold: number) => any[];
 describe("processLogs", () => {
-  // Valid logs and threshold
-  it("should throw an error when the threshold is a valid positive integer", () => {
-    const logs = ["1 2 50","1 7 70", "1 3 20", "2 2 17"]; // valid logs
-    const threshold = 2; // valid positive integer
 
-    expect(processLogsX(logs, threshold)).toStrictEqual(["1","2"]);
+  beforeEach(() => {
+    sut = processLogsX;
+  })
+
+  it("should return an array when given an array of valid logs and a threshold", () => {
+    const logs: string[] = ["1 2 50", "1 7 70", "1 3 20", "2 2 17"]; // valid logs
+    const threshold: number = 2; // valid positive integer
+    const actual: any[] = sut(logs, threshold);
+    expect(actual).toStrictEqual(["1", "2"]);
   });
 
-  // Should return an array of string ids when given an array of valid logs and a valid positive integer threshold
   it("should return an empty array when the max number of transactions is smaller than threshold", () => {
-    const logs = ["23 34 546", "45 67 890"]; // valid logs
-    const threshold = 3; // valid positive integer
-
-    expect(processLogsX(logs, threshold)).toEqual([]);
+    const logs: string[] = ["23 34 546", "45 67 890"]; // valid logs
+    const threshold: number = 3; // valid positive integer
+    const actual: any[] = sut(logs, threshold);
+    expect(actual).toEqual([]);
   });
 
-  // Should return an empty array when given an array of valid logs and a threshold that is greater than the number of logs
-  it("should return an empty array when given an array of valid logs and a threshold that is greater than the number of logs", () => {
-    const logs = ["23 34 546", "45 67 890"]; // valid logs
-    const threshold = 3; // greater than the number of logs
-
-    expect(processLogsX(logs, threshold)).toEqual([]);
+  it("should return an valid array", () => {
+    const logs: string[] = ["34 23 546", "34 23 890", "34 23 324", "34 23 865"]; // valid logs
+    const threshold: number = 3; // greater than the number of logs
+    const actual: any[] = sut(logs, threshold);
+    expect(actual).toEqual(["23","34"]);
   });
 
-  // Should return an empty array when given an empty array of logs and any threshold
-  it("should return an empty array when given an empty array of logs and any threshold", () => {
+  it("should throw an error", () => {
     const logs = []; // empty array of logs
     const threshold = 10; // any threshold
-
-    expect(processLogsX(logs, threshold)).toEqual([]);
+    const actual = () => sut(logs, threshold);
+    expect(actual).toThrow();
   });
 
-  // Should throw an error when given a threshold that is not a positive integer
-  it("should throw an error when given a threshold that is not a positive integer", () => {
-    const logs = ["23 34 546"]; // valid logs
-    const threshold = -5; // not a positive integer
-
-    expect(() => processLogsX(logs, threshold)).toThrow();
-  });
-
-  // Should throw an error when given an invalid log (missing sender_user_id, recipient_user_id, or amount_of_transactions)
-  it("should throw an error when given an invalid log (missing sender_user_id, recipient_user_id, or amount_of_transactions)", () => {
-    const logs = ["23 34", "45 67 890"]; // invalid logs
-    const threshold = 2; // valid positive integer
-
-    expect(() => processLogsX(logs, threshold)).toThrow();
-  });
-
-  // Should return an empty array when given an array of logs with only one transaction and a threshold of 2
-  it("should return an empty array when given an array of logs with only one transaction and a threshold of 2", () => {
-    const logs = ["23 34 546"]; // valid logs
-    const threshold = 2; // valid positive integer
-
-    expect(processLogsX(logs, threshold)).toEqual([]);
-  });
+  
 });
