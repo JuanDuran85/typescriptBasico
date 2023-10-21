0ed8b2e7c66563e4dba8dbfd0fae20f61e3099e3
JuanDuran85 <duran85juan@gmail.com>
2023-10-21 19:39:27 -0300

    working with test

--- test_example/ejemplo6/src/ejemplo6.ts
+++ test_example/ejemplo6/src/ejemplo6.ts
@@ -1,4 +1,48 @@
-export function miniMaxSum(arr: number[]): void {
-  // Write your code here
-  console.debug("miniMaxSum");
+export function maximumBookCopies(arr: number[]): number[] {
+  const n = arr.length;
+
+  if (n < 1 || n > 10e6) {
+    throw new Error("Error - Out of range");
+  }
+  const inventory: number[] = [];
+  const goshArr: string[] = [];
+
+  arr.forEach((value, index) => {
+    if (value > 0 && index === 0) {
+      inventory.push(1);
+      goshArr.push(`ID-${value}`);
+    }
+
+    if (
+      value > 0 &&
+      index > 0 &&
+      goshArr.includes(`ID-${value}`)
+    ) {
+      const result = goshArr.filter((a) => a.includes(`ID-${value}`));
+      inventory.push(result.length + 1);
+      goshArr.push(`ID-${value}`);
+    }
+
+    if (
+      value > 0 &&
+      index > 0 &&
+      !goshArr.includes(`ID-${value}`)
+    ) {
+      inventory.push(1);
+      goshArr.push(`ID-${value}`);
+    }
+
+    if (
+      value < 0 &&
+      index > 0 &&
+      goshArr.includes(`ID-${value*-1}`)
+    ) {
+      const result = goshArr.filter((a) => a.includes(`ID-${value * -1}`));
+      inventory.push(result.length - 1);
+      goshArr.splice(goshArr.indexOf(`ID-${value * -1}`), 1);
+    }
+  });
+
+  console.debug(inventory);
+  return inventory;
 }
--- test_example/ejemplo6/test/ejemplo6.test.ts
+++ test_example/ejemplo6/test/ejemplo6.test.ts
@@ -1,16 +1,13 @@
-import { miniMaxSum } from "../src/ejemplo6";
+import { maximumBookCopies } from "../src/ejemplo6";
 
 let sut: (arr: number[]) => void;
 
 describe("Ejemplo5 unit test suit - plusMinus", () => {
   beforeEach(() => {
-    sut = miniMaxSum;
+    sut = maximumBookCopies;
   });
   it("should throw an error when the function parameter are not an array", () => {
-    const arrIn = "string";
-    const actual: () => void = () =>
-      //@ts-ignore
-      sut(arrIn);
-    expect(actual).toThrow();
+    const actual = sut([6, 6, 2, -6, -2, -6]);
+    expect(actual).toBe([1,1,1]);
   });
 });
