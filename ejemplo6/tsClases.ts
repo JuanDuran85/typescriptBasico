50a8a5897e4b0587d1df2bc8face64ebb3090dee
JuanDuran85 <duran85juan@gmail.com>
2023-11-14 19:19:05 -0300

    decorators

---
+++ ejemplo6/tsClases.ts
@@ -0,0 +1,65 @@
+/**
+ * Decorators
+ *
+ * * A decorators in TypeScript are a special kind of feature that allows you to enhance or modify your code in a flexible way. They provide a way to attach additional functionality to classes, methods, properties, or parameters.
+ *
+ * Decorators use the form @expression, where expression must evaluate to a function that will be called at runtime with information about the decorated declaration. "TypeScript docs"
+ *
+ * A Decorator Factory is simply a function that returns the expression that will be called by the decorator at runtime. "TypeScript docs"
+ *
+ * * A Decorator Factory is a function that returns another function, which acts as the actual decorator. It allows us to configure and customize the behavior of the decorator before it is applied to a target.
+ *
+ */
+
+//------------------------------------------------------------------
+/**
+ * * Class decorators are a type of decorator that can be applied to classes.
+ * - Allow you to modify or extend the behavior of a class or its constructor, and can even replace the entire class with a new one.
+ * - They receive the class constructor as their target and can be used to add properties, methods, or modify metadata.
+ * - Class decorators are executed at runtime when the class is defined. By having access to the class constructor, they enable behavior manipulation before any instances are created.
+ * 
+ * @param target 
+ * @returns 
+ */
+
+function decoratorClassFunction<T extends new (...args: any[]) => any>(
+  target: T
+): T {
+  const OriginalConstrucutor = target;
+
+  function modifiedConstructor(...args: any[]) {
+    const instance = new OriginalConstrucutor(...args);
+
+    Object.keys(instance).forEach((key) => {
+      if (typeof instance[key] === "string") {
+        instance[key] = instance[key].toUpperCase();
+      }
+    });
+
+    return instance;
+  }
+
+  modifiedConstructor.prototype = OriginalConstrucutor.prototype;
+  return modifiedConstructor as unknown as T;
+}
+
+@decoratorClassFunction
+class ExampleClassToDecored {
+  public constructor(private name: string, private nickName: string) {}
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public displayNickName() {
+    console.debug(this.nickName);
+  }
+}
+
+const resultExample = new ExampleClassToDecored("Juan", "Juanito");
+resultExample.displayNickName();
+console.debug(resultExample.getName());
