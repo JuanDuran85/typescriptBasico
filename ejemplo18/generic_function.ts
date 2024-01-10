57aa3bd61ee6ad559d0278d8170dfd9fc2c033ec
juan-duran_cencosud <juan.duran@cencosud.cl>
2024-01-10 15:47:02 -0300

    generics functions

--- ejemplo18/generic_function.ts
+++ ejemplo18/generic_function.ts
@@ -1,16 +1,14 @@
-
-
 /*
     Funcion generica: puede recibir cualquier tipo de argumento y retornar el mismo tipo de argumento
     Por lo tanto, la funcion retornara el mismo tipo del valor enviado en el argumento, esto si se agrega en el retorno el generico
 */
 
 const genericOne = <T>(argument: T): T => {
-    return argument;
-}
+  return argument;
+};
 
 function genericTwo<T>(argument: T): T {
-    return argument;
+  return argument;
 }
 
 console.debug(genericOne(35.687435).toFixed(2));
@@ -21,19 +19,19 @@ console.debug(genericTwo(new Date()).getTime());
 /* --------------------------------------------------------------------------------- */
 
 interface Hero {
-    name: string;
-    realName: string;
+  name: string;
+  realName: string;
 }
 
 interface Villain {
-    name: string;
-    dangerlavel: number;
+  name: string;
+  dangerlavel: number;
 }
 
 const deadPoll = {
-    name: "DeadPoll",
-    realName: "Dead Poll - WWW",
-    dangerlavel: 100
-}
+  name: "DeadPoll",
+  realName: "Dead Poll - WWW",
+  dangerlavel: 100,
+};
 
 console.debug(genericOne<Hero>(deadPoll).name);
