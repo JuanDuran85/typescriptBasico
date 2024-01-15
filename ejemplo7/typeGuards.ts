4f16a70bd287d9d1766a6134cf40c5561dd985e0
JuanDuran85 <duran85juan@gmail.com>
2024-01-14 23:36:17 -0300

    style file

--- ejemplo7/typeGuards.ts
+++ ejemplo7/typeGuards.ts
@@ -1,18 +1,18 @@
 //utilizando guardian con predicado de tipo
-function isNumber2(obj:number | string) : obj is number{
-    return typeof obj === "number";
+function isNumber2(obj: number | string): obj is number {
+  return typeof obj === "number";
 }
 
-function isString2(obj:number | string) : obj is string {
-    return typeof obj === "string";
+function isString2(obj: number | string): obj is string {
+  return typeof obj === "string";
 }
 
-function printEdad(age:number | string) {
-    if (isNumber2(age)) {
-        console.info(`${age} es un numero`);
-    } else {
-        console.info(`${age} es un texto`);
-    }
+function printEdad(age: number | string) {
+  if (isNumber2(age)) {
+    console.info(`${age} es un numero`);
+  } else {
+    console.info(`${age} es un texto`);
+  }
 }
 
 printEdad(20);
--- ejemplo9/src/ejemplos/03-funciones.ts
+++ ejemplo9/src/ejemplos/03-funciones.ts
@@ -2,40 +2,40 @@
     ===== Código de TypeScript =====
 */
 
-function sumar(a:number, b:number) : number {
-    return a+b;
+function sumar(a: number, b: number): number {
+  return a + b;
 }
 
-const sumarFlecha = (a:number, b:number) : number => a+b;
+const sumarFlecha = (a: number, b: number): number => a + b;
 
-const resultado : number = sumar(4,6);
+const resultado: number = sumar(4, 6);
 console.debug(resultado);
-console.debug(sumarFlecha(3,7));
+console.debug(sumarFlecha(3, 7));
 
-function multiplicar(numA:number, numB?: number, numC: number = 5) {
-    return numA*numC;
-};
+function multiplicar(numA: number, numB?: number, numC: number = 5) {
+  return numA * numC;
+}
 
 console.debug(multiplicar(3));
 
 interface PersonajeLOR {
-    nombre: string;
-    hp: number;
-    mostrarHP: () => void;
+  nombre: string;
+  hp: number;
+  mostrarHP: () => void;
 }
 
-function curar(personaje: PersonajeLOR, curarX: number) : void {
-    personaje.hp += curarX;
-    console.debug(personaje);
-};
+function curar(personaje: PersonajeLOR, curarX: number): void {
+  personaje.hp += curarX;
+  console.debug(personaje);
+}
 
 const nuevoPersonaje: PersonajeLOR = {
-    nombre: "Juan",
-    hp: 50,
-    mostrarHP(){
-        console.debug(`Los HP son: ${this.hp}`);
-    }
+  nombre: "Juan",
+  hp: 50,
+  mostrarHP() {
+    console.debug(`Los HP son: ${this.hp}`);
+  },
 };
 
-curar(nuevoPersonaje,20);
+curar(nuevoPersonaje, 20);
 nuevoPersonaje.mostrarHP();
