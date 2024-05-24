7198f8fd90a2550a2f7c0bc220a6af17268b7a4d
JuanDuran85 <duran85juan@gmail.com>
2024-05-24 13:00:25 -0400

    Tricks for TS Exclude Utility Type

--- codes_helper/src/tricks_for_ts.ts
+++ codes_helper/src/tricks_for_ts.ts
@@ -5,19 +5,19 @@
 //---------------------------------------------------------------------------------------
 
 /**
- * 
+ *
  * Exclude is a built-in utility type in TypeScript. It is used to exclude types that you
  * do not want to include from a union type and generate a new type. This utility type is
  * very useful in development, it can help us write type-safe code and achieve better
  * code reuse.
- * 
+ *
  * https://www.typescriptlang.org/docs/handbook/utility-types.html#exclude
  */
 
 // Exclude from T those types that are assignable to U.
-type MyExclude<T, U> = T extends U ? never : T
-type T0 = MyExclude<'a' | 'b' | 'c', 'a' | 'b'>; // T0 = "c"
-type T1 = MyExclude<'a' | 'b' | 'c', 'a' | 'b' | 'd'> // T1 = "c"
+type MyExclude<T, U> = T extends U ? never : T;
+type T0 = MyExclude<"a" | "b" | "c", "a" | "b">; // T0 = "c"
+type T1 = MyExclude<"a" | "b" | "c", "a" | "b" | "d">; // T1 = "c"
 
 //* 1. Exclude specified basic data types
 
@@ -28,7 +28,6 @@ let uidOne: StringOrNumber = 1;
 uidOne = "uidTwo";
 // uidOne = true; //! Type 'boolean' is not assignable to type 'StringOrNumber'.
 console.debug(uidOne);
-
 
 //* 2. Exclude subtypes of string or number types
 
@@ -42,3 +41,43 @@ statusOut = "Not Found";
 console.debug(statusOut);
 
 //* 3. Exclude members common to two union types
+
+type TaskStatus = "Todo" | "InProgress" | "Done" | "Archived";
+type ModuleHandledStatus = "Todo" | "Done" | "OnHold";
+
+type TaskOnlyStatus = Exclude<TaskStatus, ModuleHandledStatus>; // type TaskOnlyStatus = "InProgress" | "Archived"
+
+let taskStatus: TaskOnlyStatus = "InProgress";
+taskStatus = "Archived";
+// taskStatus = "OnHold"; //! Type '"OnHold"' is not assignable to type 'TaskOnlyStatus'.
+console.debug(taskStatus);
+
+//* 4. Exclude subtypes with specific propertie
+// In this example, we want to exclude subtypes containing the legs property from the animal union type with multiple types.
+
+type AnimalType =
+  | { type: "dog"; legs: number }
+  | { type: "cat"; legs: number }
+  | { type: "fish"; fins: number };
+type AnimalsWithFins = Exclude<AnimalType, { legs: number }>;
+
+const fish: AnimalsWithFins = {
+  type: "fish",
+  fins: 5,
+};
+console.debug(fish);
+/* const dog: AnimalsWithFins = {
+    type: "dog",
+    legs: 4
+} */ //! Type '{ type: "dog"; legs: number; }' is not assignable to type 'AnimalWithFins'.
+
+//* 5. Exclude subtypes of different types of the same property
+
+// Another way to create a union type AnimalsWithFins, should be using the Exclude type via the Exclude<Animal, { type: 'dog' | 'cat'}>.
+
+type AnimalsWithFinsTwo = Exclude<AnimalType, { type: "dog" | "cat" }>;
+const fishTwo: AnimalsWithFins = {
+  type: "fish",
+  fins: 6,
+};
+console.debug(fishTwo);
