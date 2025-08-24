2112301d1c72827c472f966a8e054f834ae90864
JuanDuran85 <duran85juan@gmail.com>
2025-08-24 17:48:00 -0400

    feat: add Singleton pattern implementation and enhance DragonBalls class functionality

--- design_patterns/creationals/src/06-singleton.ts
+++ design_patterns/creationals/src/06-singleton.ts
@@ -1,11 +1,61 @@
-/**
- * ! Singleton:
- * Es un patrón de diseño creacional que garantiza que una clase
- * tenga una única instancia y proporciona un punto de acceso global a ella.
- *
- * * Es útil cuando necesitas controlar el acceso a una única instancia
- * * de una clase, como por ejemplo, en un objeto de base de datos o en un
- * * objeto de configuración.
- *
- * https://refactoring.guru/es/design-patterns/singleton
- */
+class DragonBalls {
+  private static instance: DragonBalls;
+  private ballsCollected: number;
+
+  private constructor() {
+    this.ballsCollected = 0;
+  }
+
+  public static getInstance() {
+    if (!DragonBalls.instance) {
+      DragonBalls.instance = new DragonBalls();
+      console.log("Creating new instance of DragonBalls");
+    }
+    return DragonBalls.instance;
+  }
+
+  public collectBall() {
+    if (this.ballsCollected < 7) {
+      this.ballsCollected++;
+      console.log(`You have collected ${this.ballsCollected} Dragon Ball(s).`);
+      return;
+    }
+
+    console.log("You have collected all 7 Dragon Balls!");
+  }
+
+  public summonShenlong() {
+    if (this.ballsCollected === 7) {
+      console.log("Shenlong has been summoned!");
+      this.ballsCollected = 0;
+      return;
+    }
+
+    console.log(
+      `You need to collect all 7 Dragon Balls first!. You currently have ${
+        this.ballsCollected
+      }. So, you need to collect ${7 - this.ballsCollected} more.`
+    );
+  }
+}
+
+function main() {
+  const gokuDragonBalls = DragonBalls.getInstance();
+  gokuDragonBalls.collectBall();
+  gokuDragonBalls.collectBall();
+  gokuDragonBalls.collectBall();
+  gokuDragonBalls.collectBall();
+  gokuDragonBalls.summonShenlong();
+
+  const vegetaDragonBalls = DragonBalls.getInstance();
+  vegetaDragonBalls.collectBall();
+  vegetaDragonBalls.collectBall();
+  vegetaDragonBalls.collectBall();
+  vegetaDragonBalls.collectBall();
+  vegetaDragonBalls.summonShenlong();
+
+  gokuDragonBalls.summonShenlong();
+  vegetaDragonBalls.summonShenlong();
+}
+
+main();
