0cd50996dfb174691f9831469cbd3f4d64ca0de3
JuanDuran85 <duran85juan@gmail.com>
2025-08-24 22:44:03 -0400

    feat: add Factory Function pattern implementation and example

--- design_patterns/creationals/src/07-factory-function.ts
+++ design_patterns/creationals/src/07-factory-function.ts
@@ -1,9 +1,29 @@
-/**
- * ! Factory Function
- * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
- * usados posteriormente en el código.
- *
- * * Es útil cuando necesitamos crear objetos o funciones de manera dinámica,
- * * es decir, en tiempo de ejecución y no en tiempo de compilación.
- *
- */
+type Language = "es" | "en" | "fr" | "pt";
+
+function createGreeter(lang: Language): (name: string) => string | null {
+  return (name: string): string | null => {
+    const messages: Record<string, string> = {
+      es: `Hola, ${name}`,
+      en: `Hello, ${name}`,
+      fr: `Bonjour, ${name}`,
+      pt: `Ola, ${name}`,
+    };
+
+    return messages[lang] || null;
+  };
+}
+
+function main() {
+  const spanishGreeter: (name: string) => string | null = createGreeter("es");
+  const englishGreeter: (name: string) => string | null = createGreeter("en");
+  const frenchGreeter: (name: string) => string | null = createGreeter("fr");
+  const portugueseGreeter: (name: string) => string | null =
+    createGreeter("pt");
+
+  console.log(spanishGreeter("Juan"));
+  console.log(englishGreeter("John"));
+  console.log(frenchGreeter("Jean"));
+  console.log(portugueseGreeter("João"));
+}
+
+main();
