06c9f15c7201619122c51c3e5ebcb1c851cc43b5
JuanDuran85 <duran85juan@gmail.com>
2025-09-06 22:58:11 -0400

    feat: add FireballSpell and AxeAttack abilities to the Character system

--- design_patterns/structural/src/02-bridge.ts
+++ design_patterns/structural/src/02-bridge.ts
@@ -9,9 +9,21 @@ class SwordAttack implements Ability {
   }
 }
 
+class FireballSpell implements Ability {
+  public use(): void {
+    console.debug("%cCasting a fireball spell", COLORS.yellow);
+  }
+}
+
 class MagicSpell implements Ability {
   public use(): void {
     console.debug("%cCasting a magic spell", COLORS.blue);
+  }
+}
+
+class AxeAttack implements Ability {
+  public use(): void {
+    console.debug("%cAttacking with an axe", COLORS.green);
   }
 }
 
@@ -35,3 +47,33 @@ class PrincipalWarriorActor extends Character {
     this.ability.use();
   }
 }
+
+class MageActor extends Character {
+  override performAbility(): void {
+    console.debug("Mage performing ability...");
+    this.ability.use();
+  }
+}
+
+function main() {
+  const warriorActor: PrincipalWarriorActor = new PrincipalWarriorActor(
+    new SwordAttack()
+  );
+
+  warriorActor.performAbility();
+  warriorActor.setAbility(new MagicSpell());
+  warriorActor.performAbility();
+  warriorActor.setAbility(new AxeAttack());
+  warriorActor.performAbility();
+  console.debug("\n");
+
+  const mageActor = new MageActor(new AxeAttack());
+  mageActor.performAbility();
+  mageActor.setAbility(new MagicSpell());
+  mageActor.performAbility();
+  mageActor.setAbility(new FireballSpell());
+  mageActor.performAbility();
+  console.debug("\n");
+}
+
+main();
