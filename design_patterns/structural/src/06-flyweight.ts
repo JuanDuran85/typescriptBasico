acd53b043628a0412db11d17bc2354127fa6572f
JuanDuran85 <duran85juan@gmail.com>
2025-09-26 18:25:12 -0300

    Refactor LocationFlyWeightFactory and MapLocation classes for improved clarity and organization

--- design_patterns/structural/src/06-flyweight.ts
+++ design_patterns/structural/src/06-flyweight.ts
@@ -28,6 +28,7 @@ class LocationFlyWeightFactory {
 
   public getLocationIcon(type: string): LocationIcon {
     if (!this.icons[type]) {
+      console.debug(`%cCreating new icon of type: ${type}`, COLORS.red);
       const iconImage = `icon-${type}.png`;
       this.icons[type] = new LocationIcon(type, iconImage);
     }
@@ -35,3 +36,35 @@ class LocationFlyWeightFactory {
     return this.icons[type];
   }
 }
+
+class MapLocation {
+  private coordinates: { x: number; y: number };
+  private icon: LocationIcon;
+
+  public constructor(x: number, y: number, icon: LocationIcon) {
+    this.coordinates = { x, y };
+    this.icon = icon;
+  }
+
+  public display(): void {
+    this.icon.display(this.coordinates);
+  }
+}
+
+function main() {
+  const factory: LocationFlyWeightFactory = new LocationFlyWeightFactory();
+
+  const locations: MapLocation[] = [
+    new MapLocation(10, 20, factory.getLocationIcon("hospital")),
+    new MapLocation(43, 45, factory.getLocationIcon("hospital")),
+    new MapLocation(23,67, factory.getLocationIcon("school")),
+    new MapLocation(56,78, factory.getLocationIcon("school")),
+    new MapLocation(34,89, factory.getLocationIcon("restaurant")),
+    new MapLocation(90,12, factory.getLocationIcon("restaurant")),
+    new MapLocation(11,22, factory.getLocationIcon("restaurant")),
+  ];
+
+  locations.forEach((location: MapLocation) => location.display());
+}
+
+main();
