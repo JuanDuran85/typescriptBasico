b44eed1452fd6f1b52d62687bcbb77cbc5c5a510
JuanDuran85 <duran85juan@gmail.com>
2025-10-27 17:18:46 -0300

    Add AdvancedSupport and ExpertSupport handlers with colored console output

--- design_patterns/behavioral/src/01-chain-responsibility.ts
+++ design_patterns/behavioral/src/01-chain-responsibility.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "./helpers/colors.ts";
+
 interface Handler {
   setNext(handler: Handler): Handler;
   handle(request: string): void;
@@ -19,13 +21,50 @@ abstract class AbstractBaseHandler implements Handler {
 class BasicSupport extends AbstractBaseHandler {
   public override handle(request: string): void {
     if (request === "basic") {
-      console.debug("Basic Support: Resolving issues");
+      console.debug("Basic Support: Resolving issues", COLORS.green);
       return;
     }
 
     console.debug(
-      "Basic Support: We can not handler this issue. Pass to Advance support"
+      "Basic Support: We can not handler this issue. Pass to Advance support",
+      COLORS.red
     );
     super.handle(request);
   }
 }
+
+class AdvancedSupport extends AbstractBaseHandler {
+  public override handle(request: string): void {
+    if (request === "advanced") {
+      console.debug("Advanced Support: Resolving issues", COLORS.yellow);
+      return;
+    }
+
+    console.debug(
+      "Advanced Support: We can not handler this issue. Pass to Next support (Expert)",
+      COLORS.red
+    );
+    super.handle(request);
+  }
+}
+
+class ExpertSupport extends AbstractBaseHandler {
+  public override handle(request: string): void {
+    if (request === "expert") {
+      console.debug("Expert Support: Resolving issues", COLORS.blue);
+      return;
+    }
+
+    console.debug(
+      "Expert Support: We can not handler this issue. We are sorry",
+      COLORS.red
+    );
+    super.handle(request);
+  }
+}
+
+function main() {
+  
+}
+
+main();
