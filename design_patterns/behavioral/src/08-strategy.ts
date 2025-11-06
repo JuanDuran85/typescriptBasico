ff3e6bbad9b6d7920dabbb1e638101c2d1ab1bfa
JuanDuran85 <duran85juan@gmail.com>
2025-11-06 12:51:05 -0300

    feat: enhance MovementStrategy implementations with color-coded console messages

--- design_patterns/behavioral/src/08-strategy.ts
+++ design_patterns/behavioral/src/08-strategy.ts
@@ -1,21 +1,48 @@
+import { COLORS } from "./helpers/colors.ts";
+
 interface MovementStrategy {
   move(): void;
 }
 
 class SwimFast implements MovementStrategy {
   public move(): void {
-    console.debug("The duck isSwimming fast!");
+    console.debug("%cThe duck isSwimming fast!", COLORS.cyan);
   }
 }
 
 class FlyOverWater implements MovementStrategy {
   public move(): void {
-    console.debug("The duck is flying over the water!");
+    console.debug("%cThe duck is flying over the water!", COLORS.blue);
   }
 }
 
 class WalkingClumsily implements MovementStrategy {
   public move(): void {
-    console.debug("The duck is walking clumsily!");
+    console.debug("%cThe duck is walking clumsily!", COLORS.brown);
+  }
+}
+
+class Duck {
+  private name: string;
+  private movementStrategy: MovementStrategy;
+
+  constructor(name: string, movementStrategy: MovementStrategy) {
+    this.name = name;
+    this.movementStrategy = movementStrategy;
+
+    console.debug("%cReady duck: ", COLORS.green, this.name);
+  }
+
+  public performMove(): void {
+    console.debug(
+      `The duck: %c${this.name} %cis ready to move`,
+      COLORS.green,
+      COLORS.white
+    );
+  }
+
+  public setMovementStrategy(movementStrategy: MovementStrategy): void {
+    this.movementStrategy = movementStrategy;
+    console.debug(`${this.name} has been changed the strategy`);
   }
 }
