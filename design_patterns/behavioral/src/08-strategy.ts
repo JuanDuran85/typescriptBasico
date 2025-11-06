108e4d70062b859dc4172045f84bf56cd8321989
JuanDuran85 <duran85juan@gmail.com>
2025-11-06 12:57:31 -0300

    fix: ensure duck performs movement during status display

--- design_patterns/behavioral/src/08-strategy.ts
+++ design_patterns/behavioral/src/08-strategy.ts
@@ -39,6 +39,7 @@ class Duck {
       COLORS.green,
       COLORS.white
     );
+    this.movementStrategy.move();
   }
 
   public setMovementStrategy(movementStrategy: MovementStrategy): void {
@@ -46,3 +47,26 @@ class Duck {
     console.debug(`${this.name} has been changed the strategy`);
   }
 }
+
+function main() {
+  const duck01: Duck = new Duck("Daisy SwimFast", new SwimFast());
+  const duck02: Duck = new Duck("Donald FlyOverWater", new FlyOverWater());
+  const duck03: Duck = new Duck("Daffy WalkingClumsily", new WalkingClumsily());
+
+  console.debug("--------- Start Game ---------");
+  duck01.performMove();
+  duck02.performMove();
+  duck03.performMove();
+
+  console.debug("--------- Change Strategies ---------");
+  duck01.setMovementStrategy(new FlyOverWater());
+  duck02.setMovementStrategy(new WalkingClumsily());
+  duck03.setMovementStrategy(new SwimFast());
+
+  console.debug("--------- Continue Game ---------");
+  duck01.performMove();
+  duck02.performMove();
+  duck03.performMove();
+}
+
+main();
